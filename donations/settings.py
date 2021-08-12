6a96e99832796537ccc6de7b299f4380bd0b0f31
Alex <49983070+AlexanderHalsey@users.noreply.github.com>
2021-08-12 21:47:51 +0200

    m

--- donations/settings.py
+++ donations/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = (
+    
+)
 
 STATIC_URL = '/static/'
 
