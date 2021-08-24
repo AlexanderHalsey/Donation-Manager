7bd1dd80d409b6c2cbbcd756c783012d7a48613f
AlexanderHalsey <49983070+AlexanderHalsey@users.noreply.github.com>
2021-08-24 17:43:53 +0200

    new webhook url

--- dm_page/urls.py
+++ dm_page/urls.py
@@ -4,7 +4,7 @@ from dm_page import views
 urlpatterns = [
 	path('login/', views.loginUser, name='login'),
 	path('logout/', views.logoutUser, name='logout'),
-	path('webhooks/', views.webhooks),
+	path('api/webhooks/', views.webhooks),
 	path('', views.dashboard, name='dashboard'),
 	path('donators/', views.donators, name="donators"),
 	path('contact/<str:pk>/', views.contact, name='contact'),
