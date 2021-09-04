e6040443190dee87ebe9b00835e137cf36c5d293
AlexanderHalsey <49983070+AlexanderHalsey@users.noreply.github.com>
2021-09-04 19:50:45 +0200

    table fix, language for seminar desk button, filter disabled=False for contacts

--- dm_page/forms.py
+++ dm_page/forms.py
@@ -13,8 +13,8 @@ class DonationForm(forms.Form):
 
 	def __init__(self, *args, **kwargs):
 		super(DonationForm, self).__init__(*args, **kwargs)
-		self.fields['contact'].choices = [(str(contact), str(contact)) for contact in Contact.objects.all()]
+		self.fields['contact'].choices = [(str(contact), str(contact)) for contact in Contact.objects.filter(profile__disabled=False)]
 		self.fields['payment_mode'].choices = [(str(mode), str(mode)) for mode in PaymentMode.objects.all()]
 		self.fields['donation_type'].choices = [(str(t), str(t)) for t in DonationType.objects.all()]
-		self.fields['organisation'].choices = [(str(organisation), str(organisation)) for organisation in Organisation.objects.all()]
+		self.fields['organisation'].choices = [(str(organisation), str(organisation)) for organisation in Organisation.objects.filter(profile__disabled=False)]
 
