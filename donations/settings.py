61d2a19a8faa9a4ede55c6dda249887a21565164
AlexanderHalsey <49983070+AlexanderHalsey@users.noreply.github.com>
2021-09-04 13:48:32 +0200

    django_heroku

--- donations/settings.py
+++ donations/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from dotenv import load_dotenv
+import django_heroku
 
 load_dotenv()
 
@@ -150,3 +151,5 @@ CACHES = {
         'LOCATION': 'my_cache_table',
     }
 }
+
+django_heroku.settings(locals())
