7a976266af1a08737b8c541184f3612fdc7c3ca3
AlexanderHalsey <49983070+AlexanderHalsey@users.noreply.github.com>
2021-09-10 19:53:48 +0200

    add herokuapp to staticfiles dirs

--- donations/settings.py
+++ donations/settings.py
@@ -151,6 +151,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    'dmsivy.herokuapp.com/static',
     )
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/pdf/receipts')
 MEDIA_URL = '/receipts/'
