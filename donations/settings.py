08bd7c7a4315a705b6d0b6fe9167cfbdb5d1112f
AlexanderHalsey <49983070+AlexanderHalsey@users.noreply.github.com>
2021-09-10 20:01:54 +0200

    staticfilesdirs

--- donations/settings.py
+++ donations/settings.py
@@ -149,10 +149,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    'dmsivy.herokuapp.com/static',
-    )
+    "dmsivy.herokuapp.com/static",
+    ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/pdf/receipts')
 MEDIA_URL = '/receipts/'
 
